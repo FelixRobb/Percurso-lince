982d84814412c4c18bd798130a8c2c83d7d47808
flx.nns.rrb <flx.nns.rrb@gmail.com>
2024-07-09 17:52:20 +0000

    g

--- popup.js
+++ popup.js
@@ -1,11 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const popupCloseButton = document.getElementById('popup-close');
-    
-    // Always show the popup
-    document.getElementById('popup').style.display = 'block';
+        // Get the modal
+        var modal = document.getElementById("myModal");
 
-    popupCloseButton.addEventListener('click', () => {
-        // Redirect to map.html
-        window.location.href = 'map.html';
-    });
-});
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // Get the OK button
+        var okButton = document.getElementById("modal-ok-button");
+
+        // When the user clicks the OK button, close the modal
+        okButton.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        // Display the modal when the page loads
+        window.onload = function() {
+            modal.style.display = "block";
+        }
