6e15c7050e189eac3a220c114ca6e9a91696dae7
FelixRobb <felix.nunes.robb@gmail.com>
2024-07-19 12:32:36 +0100

    "Added info popups to index.html, updated popup.js to handle opening and closing popups, and added styles for info-modal in styles.css."

--- popup.js
+++ popup.js
@@ -1,30 +1,47 @@
-        // Get the modal
-        var modal = document.getElementById("myModal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-        // Get the OK button
-        var okButton = document.getElementById("modal-ok-button");
+// Get the OK button
+var okButton = document.getElementById("modal-ok-button");
 
-        // When the user clicks the OK button, close the modal
-        okButton.onclick = function() {
-            modal.style.display = "none";
-        }
+// When the user clicks the OK button, close the modal
+okButton.onclick = function () {
+    modal.style.display = "none";
+}
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-        // Display the modal when the page loads
-        window.onload = function() {
-            modal.style.display = "block";
-        }
+// Display the modal when the page loads
+window.onload = function () {
+    modal.style.display = "block";
+}
+
+// Open popup
+function openInfoPopup(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+// Close popup
+function closeInfoPopup(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+// Close the popup when clicking outside of it
+window.onclick = function (event) {
+    if (event.target.classList.contains('info-modal')) {
+        event.target.style.display = 'none';
+    }
+}
